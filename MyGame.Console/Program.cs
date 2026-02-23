a69fa1481c8638b5a995cbd1d31487e086da1e78
Alexander Zhgarev <jgarevalexandr@yandex.ru>
2026-02-24 01:25:24 +0300

    Implemented the "Game Loop"

--- MyGame.Console/Program.cs
+++ MyGame.Console/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Goodbye, World!");
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static bool isRunning = true;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the game! Press Esc to exit.");
+
+        while (isRunning)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.Escape)
+                {
+                    isRunning = false;
+                }
+            }
+
+            if (isRunning)
+            {
+                Update();
+                Data();
+            }
+
+            Thread.Sleep(16);
+        }
+
+        Console.WriteLine("Goodbye!");
+    }
+
+    static void Update()
+    {
+    
+    }
+
+    static void Data()
+    {
+    
+    }
+}
