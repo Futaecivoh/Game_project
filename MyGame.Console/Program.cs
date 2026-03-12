045750d02cd9d4205cf572a64fd727a5a7c72bfc
Futaecivoh <68806585+Futaecivoh@users.noreply.github.com>
2026-03-12 17:00:45 +0300

    add Encoding

--- MyGame.Console/Program.cs
+++ MyGame.Console/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Threading;
+using System.Text;
 
 class Program
 {
     static void Main(String[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
+
         GameManager.Instance.Run();
     }
 }
